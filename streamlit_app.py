6c2de27de71aa93479128ce1016e7ff0c2d52201
whitecatwedsite <whitecatanimation@gmail.com>
2024-08-23 21:48:33 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
+st.text_input('WE LIVE IN A TWILIGHT WORLD:')
+st.title("Welcome back agent::::::::")
+st.write("Name -Agent Major Dagm::::::::::::::::" )   
+st.write("Rank - s+ tre:::::::::::")
+st.write("Kill count - 879:::::::::::")
+st.write("Mission done - 92::::::::::::::")
+st.write("Next mission - meet up with agent ben::::::::::::")
+st.write("Story - he is one of the best agent(the only agnt)in the crew. ::::::::::::::")
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
